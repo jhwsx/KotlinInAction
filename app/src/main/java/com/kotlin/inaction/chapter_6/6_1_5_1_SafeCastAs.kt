31286d03fbf498709e4ab0c2609d3c0cf800bd01
wangzhichao <392337950@qq.com>
2019-03-02 00:13:58 +0800

    6.1.5

---
+++ app/src/main/java/com/kotlin/inaction/chapter_6/6_1_5_1_SafeCastAs.kt
@@ -0,0 +1,33 @@
+package com.kotlin.inaction.chapter_6
+
+/**
+ *
+ * @author wzc
+ * @date 2019/3/1
+ */
+fun boo(x: Any) {
+    val number = x as Number
+    println(number)
+}
+
+fun boo1(x: Any) {
+    val number = x as? Number
+    println(number)
+}
+
+fun boo2(x: Any) {
+    if (x is Number) {
+        println(x)
+    } else {
+        println(null)
+    }
+}
+fun main(args: Array<String>) {
+    boo1(5)
+    boo1("b")
+    boo2(5)
+    boo2("b")
+    boo(5)
+    boo("b")
+
+}
