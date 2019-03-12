593278b4e8f2b33aa57617e47a769ceb9d2e3a02
wangzhichao <392337950@qq.com>
2019-03-12 23:25:15 +0800

    6.1.8

---
+++ app/src/test/java/com/kotlin/inaction/chapte_6/MyTest.kt
@@ -0,0 +1,22 @@
+package com.kotlin.inaction.chapte_6
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+/**
+ *
+ * @author wzc
+ * @date 2019/3/12
+ */
+class MyTest {
+    private var myService: MyService? = null
+
+    @Before fun setUp() {
+        myService = MyService()
+    }
+
+    @Test fun testAction() {
+        Assert.assertEquals("foo", myService!!.performAction())
+    }
+}
