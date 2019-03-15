803b2a498a0db6d709d89749acd61f998d1d1cfb
wangzhichao <392337950@qq.com>
2019-03-16 00:20:44 +0800

    6.2.3

---
+++ app/src/main/java/com/kotlin/inaction/chapter_6/6_2_3_2_NumberConversions.kt
@@ -0,0 +1,34 @@
+package com.kotlin.inaction.chapter_6
+
+/**
+ *
+ * @author wzc
+ * @date 2019/3/14
+ */
+fun main(args: Array<String>) {
+    val l: Long = 123L
+    println(l)
+    val d: Double = 0.123
+    println(d)
+    val d2: Double = 123.0
+    println(d2)
+    val d3: Double = 1.23e10
+    println(d3)
+    val d4: Double = 1.23e-10
+    println(d4)
+    val f: Float = 123.4f
+    println(f)
+    val f1: Float = .456F
+    println(f1)
+    val f2: Float = 1e3f
+    println(f2)
+    val hex1: Int = 0xABCDE
+    println(hex1)
+    val hex2: Int = 0xabcde
+    println(hex2)
+    val b: Int = 0b110
+    println(b)
+    val b2: Int = 0B001
+    println(b2)
+
+}
---
+++ app/src/main/java/com/kotlin/inaction/chapter_6/6_2_3_3_NumberConversions.kt
@@ -0,0 +1,28 @@
+package com.kotlin.inaction.chapter_6
+
+/**
+ *
+ * @author wzc
+ * @date 2019/3/14
+ */
+fun main(args: Array<String>) {
+    val b: Byte = 1 // 使用数字字面值去初始化一个类型已知的变量时
+    val l = b + 1L
+    foo(l) // 把字面值作为实参传递给函数
+
+    foo(42)
+
+}
+
+fun foo(l: Long) {
+    println(l)
+}
+
+/**
+ * 总结：
+ * 1，自动转换发生的情况:
+ * 使用数字字面值去初始化一个类型已知的变量时，
+ * 把字面值作为实参传递给函数时。
+ * 2, + 可以进行字节类型和长整型参数的计算
+ *
+ */
