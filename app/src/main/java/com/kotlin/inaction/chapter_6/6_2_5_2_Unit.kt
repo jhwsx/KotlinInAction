c0e2cc603609e610803e9cb0210995cab6fcb354
wangzhichao <392337950@qq.com>
2019-03-17 09:24:18 +0800

    62.5

---
+++ app/src/main/java/com/kotlin/inaction/chapter_6/6_2_5_2_Unit.kt
@@ -0,0 +1,32 @@
+package com.kotlin.inaction.chapter_6
+
+/**
+ *
+ * @author wzc
+ * @date 2019/3/17
+ */
+interface Processor<T> {
+    fun process(): T
+}
+
+class NoResultProcessor : Processor<Unit> {
+    override fun process(): Unit {
+        println("NoResultProcessor process()")
+        return Unit
+    }
+}
+
+class ResultProcessor : Processor<String> {
+    override fun process(): String {
+        return "ResultProcessor process()"
+    }
+}
+
+fun main(args: Array<String>) {
+    val noResultProcessor = NoResultProcessor()
+    noResultProcessor.process()
+
+    val resultProcessor = ResultProcessor()
+    val process = resultProcessor.process()
+    println(process)
+}
---
+++ app/src/main/java/com/kotlin/inaction/chapter_6/UnitSampleEx.java
@@ -0,0 +1,17 @@
+package com.kotlin.inaction.chapter_6;
+
+/**
+ * @author wzc
+ * @date 2019/3/17
+ */
+public class UnitSampleEx extends UnitSample {
+    @Override
+    public void f() {
+        System.out.println("UnitSampleEx f()");
+    }
+
+    @Override
+    public void g() {
+        System.out.println("UnitSampleEx g()");
+    }
+}
