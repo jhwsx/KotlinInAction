a0ec95e729e25805b4ead693ffb46d26fa9d3dee
wangzhichao <392337950@qq.com>
2019-03-17 09:41:33 +0800

    6.2.6

---
+++ app/src/main/java/com/kotlin/inaction/chapter_6/6_2_6_1_Nothing.kt
@@ -0,0 +1,32 @@
+package com.kotlin.inaction.chapter_6
+
+/**
+ *
+ * @author wzc
+ * @date 2019/3/17
+ */
+fun fail(message: String): Nothing {
+    throw IllegalStateException(message)
+}
+
+fun main(args: Array<String>) {
+//    fail("Error occured")
+
+    val firm = Firm("wzc", null)
+    val location1 = firm.location ?: throw IllegalStateException("location is null")
+    val location2 = firm.location ?: flunk()
+    val location3 = firm.location ?: fail("location is null")
+    println(location3.city)
+}
+
+private fun flunk() {
+    throw IllegalStateException("location is null")
+}
+
+/**
+ * 总结：
+ * 1，Nothing 类型没有任何值，查看一下看见 Nothing 是一个默认构造被私有化的类；
+ * 2，Nothing 的用处：当做函数返回值，表示函数永远不会正常终止；
+ * 当作泛型函数返回值的类型参数。
+ * 3，对比一下：flunk() 和 fail() 方法，因为它们的返回值不同，造成了 location2 和 location3 它们类型推断也不一样。
+ */
