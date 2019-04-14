2c5e1da06b1ade31af26036ee49364b25fd8b4db
wangzhichao <wangzhichao@adups.com>
2019-04-14 15:55:39 +0800

    7.4.1

---
+++ app/src/main/java/com/kotlin/inaction/chapter_7/7_4_1_1_DestructuringDeclarationsAndLoops.kt
@@ -0,0 +1,33 @@
+package com.kotlin.inaction.chapter_7
+
+/**
+ *
+ * @author wzc
+ * @date 2019/4/14
+ */
+fun printEntries(map: Map<String, String>) {
+    for ((key, value) in map) {
+        println("$key -> $value")
+    }
+}
+
+fun main() {
+    val map = mapOf(
+        "Z40" to "Shanghai-Zhengzhou",
+        "K154" to "Zhengzhou-Shanghai"
+    )
+    printEntries(map)
+}
+
+/**
+ * 总结：
+ * 1, 解构声明调用的是  Maps.kt 中的
+ *  @kotlin.internal.InlineOnly
+ *  public inline operator fun <K, V> Map.Entry<K, V>.component1(): K = key
+ *
+ *  @kotlin.internal.InlineOnly
+ *  public inline operator fun <K, V> Map.Entry<K, V>.component2(): V = value
+ * 2, Map 能够使用 in 运算符是因为有一个定义好的 iterator 扩展函数。
+ * @kotlin.internal.InlineOnly
+ * public inline operator fun <K, V> Map<out K, V>.iterator(): Iterator<Map.Entry<K, V>> = entries.iterator()
+ */
