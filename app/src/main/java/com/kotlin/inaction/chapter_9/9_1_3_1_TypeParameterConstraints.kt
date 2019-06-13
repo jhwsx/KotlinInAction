9847a42d2c299eb7e115d7c85281da1edd34898d
wangzhichao <392337950@qq.com>
2019-06-14 00:30:20 +0800

    9.1.3.1

---
+++ app/src/main/java/com/kotlin/inaction/chapter_9/9_1_3_1_TypeParameterConstraints.kt
@@ -0,0 +1,12 @@
+package com.kotlin.inaction.chapter_9
+
+/**
+ *
+ * @author wzc
+ * @date 2019/6/14
+ */
+fun main(args: Array<String>) {
+    println(listOf(1, 2, 3).sum())
+    println(listOf(1.1, 2.2, 3.3).sum())
+//    listOf("a","b","c").sum()
+}
