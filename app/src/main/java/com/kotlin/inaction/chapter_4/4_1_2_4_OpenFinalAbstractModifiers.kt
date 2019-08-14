66386b1d47bf81f18ba5281742af9a3c6a5e9c04
wangzhichao <392337950@qq.com>
2019-08-15 00:01:18 +0800

    4.1.2 review

---
+++ app/src/main/java/com/kotlin/inaction/chapter_4/4_1_2_4_OpenFinalAbstractModifiers.kt
@@ -0,0 +1,13 @@
+package com.kotlin.inaction.chapter_4
+
+/**
+ *
+ * @author wzc
+ * @date 2019/8/15
+ */
+//open data class X(val name: String)
+
+/**
+ * 总结：
+ * Modifier 'open' is incompatible with 'data'
+ */
