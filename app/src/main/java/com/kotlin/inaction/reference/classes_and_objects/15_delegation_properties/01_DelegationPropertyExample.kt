e514b5803795f3ff83760ea3fb888ef960ad04ec
wangzhichao <wangzhichao@adups.com>
2019-09-06 22:49:18 +0800

    classes_and_objects-delegation_properties

---
+++ app/src/main/java/com/kotlin/inaction/reference/classes_and_objects/15_delegation_properties/01_DelegationPropertyExample.kt
@@ -0,0 +1,39 @@
+package com.kotlin.inaction.reference.classes_and_objects.`15_delegation_properties`
+
+import kotlin.reflect.KProperty
+
+class Example {
+    var p: String by Delegate()
+//        get()= field
+//        set(value) {field = value}
+
+}
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("$value has been assigned to '${property.name}' in $thisRef")
+
+    }
+}
+
+fun main(args: Array<String>) {
+    val e = Example()
+    println(e.p) // e.p 会调用 Delegate 类的 getValue() 方法
+    e.p = "you're welcome." // 会调用 Delegate 类的 setValue() 方法
+}
+
+/*
+打印结果：
+com.kotlin.inaction.reference.classes_and_objects.15_delegation_properties.Example@3af49f1c, thank you for delegating 'p' to me!
+you're welcome. has been assigned to 'p' in com.kotlin.inaction.reference.classes_and_objects.15_delegation_properties.Example@3af49f1c
+*/
+/**
+ * 总结：
+ * 1，委托属性的语法：val/var <property name>: <Type> by <expression>
+ *  委托属性的 get() 或者 set() 方法会被委托给委托类的 getValue() 或者 setValue() 方法。
+ * 2，从打印结果可以知道：getValue，setValue 方法的第一个参数 thisRef 就是 Example 对象
+ */
