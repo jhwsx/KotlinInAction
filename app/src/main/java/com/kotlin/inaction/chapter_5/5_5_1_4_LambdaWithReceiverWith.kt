d921bd55c1bc3971fc43d4cdaf7c55c19228c82e
wangzhichao <392337950@qq.com>
2020-09-01 01:20:24 +0800

    review chapter5

--- app/src/main/java/com/kotlin/inaction/chapter_5/5_5_1_4_LambdaWithReceiverWith.kt
+++ app/src/main/java/com/kotlin/inaction/chapter_5/5_5_1_4_LambdaWithReceiverWith.kt
@@ -11,6 +11,18 @@ fun main(args: Array<String>) {
         println("'with' is called with argument $this")
         println("It contains $size elements")
     }
+    println("---------------------------------------")
+    val block: (MutableList<String>).() -> Unit = {
+        println("'with' is called with argument $this")
+        println("It contains $size elements")
+    }
+    with(numbers, block)
+    println("---------------------------------------")
+    val block2: (MutableList<String>).() -> Unit = fun (MutableList<String>).() : Unit {
+        println("'with' is called with argument $this")
+        println("It contains $size elements")
+    }
+    with(numbers, block2)
 }
 
 /**
