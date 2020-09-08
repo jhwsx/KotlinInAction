2ffa7184d75710d1c03fb519b49053c7f1715baf
wangzhichao <392337950@qq.com>
2020-09-08 23:48:41 +0800

    review chapter6

--- app/src/main/java/com/kotlin/inaction/chapter_6/6_1_1_1_NullableTypes.kt
+++ app/src/main/java/com/kotlin/inaction/chapter_6/6_1_1_1_NullableTypes.kt
@@ -1,5 +1,7 @@
 package com.kotlin.inaction.chapter_6
 
+import java.util.*
+
 /**
  *
  * @author wzc
@@ -15,7 +17,7 @@ fun strLen(s: String) = s.length
  * 这个函数允许传入 null 的实参，它自己处理了这种情况
  */
 fun strLenSafe(s: String?): Int =
-        if (s != null) s.length else 0
+        if (s != null) s.length else 0 // 通过比较后，s 就当作非空对待了。
 
 //fun strLenSafe2(s: String?) : Int = s.length // 不能拿一个可空类型的值去调用自己的函数
 
@@ -37,4 +39,5 @@ fun main(args: Array<String>) {
  * 2，任何类型后面没有问号表示这种类型的变量不能存储 null 引用。
  * 3，如果值是可空类型，那么它进行的操作会有限制：
  * 不能再调用它的方法，不能把它赋值给非空类型的变量，不能把可空类型的值传给拥有非空类型参数的函数
+ * 4, 可空类型最重要的操作就是和 null 比较。在比较发生的作用域内把这个值当做非空对待。
  */
