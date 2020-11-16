0ea6755bb5eb4a6d182914d136f1ff6e2b36b1a9
wangzhichao <willwaywang6@gmail.com>
2020-11-16 15:19:10 +0800

    review chapter7

---
+++ app/src/main/java/com/kotlin/inaction/chapter_7/ListWithTrash.kt
@@ -0,0 +1,23 @@
+package com.kotlin.inaction.chapter_7
+
+/**
+ * 类代理
+ *
+ * 反编译的代码里用到了装饰器模式
+ *
+ * @author wangzhichao
+ * @date 20-11-13
+ */
+class ListWithTrash<T>(
+        private val innerList: MutableList<T> = ArrayList<T>()
+) : MutableCollection<T> by innerList {
+    var deletedItem: T? = null
+    override fun remove(element: T): Boolean {
+        deletedItem = element
+        return innerList.remove(element)
+    }
+
+    fun recover(): T? {
+        return deletedItem
+    }
+}
